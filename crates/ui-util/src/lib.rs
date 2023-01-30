961fb52dbf31d27d51d1470333de2602f165df0c
Axel Lord <axel.rinkaby@gmail.com>
2023-01-30 22:13:30 +0100

    Retought some stuff

--- crates/ui-util/src/lib.rs
+++ crates/ui-util/src/lib.rs
@@ -70,6 +70,34 @@ pub enum Theme {
     DarkMute,
 }
 
+impl Theme {
+    /// Get [`BoxPalette`] representing current theme.
+    #[must_use]
+    pub fn box_palette(&self) -> BoxPalette {
+        let ContrastPalette { bright, dim } = self.contrast_palette();
+        match self {
+            Theme::Light => BoxPalette {
+                text: dim,
+                background: bright,
+            },
+            Theme::Dark => BoxPalette {
+                text: bright,
+                background: dim,
+            },
+            Theme::DarkMute => BoxPalette {
+                text: bright,
+                background: mute_color(dim, None),
+            },
+        }
+    }
+
+    /// Get [`ContrastPalette`] representing current theme.
+    #[must_use]
+    pub fn contrast_palette(&self) -> ContrastPalette {
+        ContrastPalette::monochrome()
+    }
+}
+
 /// Configuration for boxes.
 #[derive(Clone, Copy, Debug)]
 pub struct BoxOptions {
@@ -195,10 +223,13 @@ impl ContrastPalette {
     }
 }
 
+/// A Palette with values for foreground/text and background.
 #[derive(Debug, Clone, Copy)]
-struct BoxPalette {
-    text: Color,
-    background: Color,
+pub struct BoxPalette {
+    /// Foreground/text/outline color.
+    pub text: Color,
+    /// Background color.
+    pub background: Color,
 }
 
 fn lerp(a: f32, b: f32, t: f32) -> f32 {
@@ -217,7 +248,9 @@ fn mute_color(Color { r, g, b, a }: Color, t: Option<f32>) -> Color {
 }
 
 impl BoxPalette {
-    fn from_contrast_palette(theme: Theme, palette: ContrastPalette) -> Self {
+    /// Create a [`BoxPalette`] from a [`ContrastPalette`] and a theme.
+    #[must_use]
+    pub fn from_contrast_palette(theme: Theme, palette: ContrastPalette) -> Self {
         match theme {
             Theme::Light => BoxPalette {
                 text: palette.dim,
@@ -234,7 +267,9 @@ impl BoxPalette {
         }
     }
 
-    fn invert(self) -> Self {
+    /// Swap background and foreground.
+    #[must_use]
+    pub fn invert(self) -> Self {
         let Self { text, background } = self;
         Self {
             text: background,
@@ -242,7 +277,9 @@ impl BoxPalette {
         }
     }
 
-    fn mute_background(self, t: Option<f32>) -> Self {
+    /// Mute the background color somewhat.
+    #[must_use]
+    pub fn mute_background(self, t: Option<f32>) -> Self {
         let Self { text, background } = self;
         Self {
             text,
@@ -260,69 +297,37 @@ impl Default for ContrastPalette {
 pub mod theme {
     //! Different themes for widgets.
 
-    use crate::{BoxOptions, ContrastPalette};
-    use paste::paste;
+    use crate::{BoxOptions, BoxPalette, ContrastPalette, Theme};
+    use bookmark_util::Somewhere;
+    use iced::Color;
+    // use paste::paste;
 
-    /// Container theme, used in some way by many widgets.
-    #[derive(Clone, Copy, Debug, Default)]
-    pub struct Container {
-        /// Colors used by container.
-        pub palette: ContrastPalette,
-        /// Options used by container.
-        pub options: BoxOptions,
+    /// Style used for [Container][iced::widget::Container] widgets
+    #[derive(Default)]
+    pub enum Container {
+        /// Use the default style of the current theme.
+        #[default]
+        Theme,
+        /// Use a palette based on contrast swapping what is foreground and background based on
+        /// theme
+        ContrastPalette(ContrastPalette, BoxOptions),
+        /// Use a palette where the foreground and background values are set.
+        BoxPalette(BoxPalette, BoxOptions),
+        /// Implement the style yourself and pass it.
+        Custom(Somewhere<dyn iced::widget::container::StyleSheet<Style = Theme>>),
     }
 
-    macro_rules! container_presets {
-        ($(($p_preset:ident, $o_preset:ident)),+ $(,)?) => {
-            paste! {$(
-                #[must_use]
-                #[doc = "Preset with " $o_preset " options and " $p_preset " colors."]
-                pub fn [<$p_preset _ $o_preset>] () -> Self {
-                    Self {
-                        palette: ContrastPalette:: $p_preset (),
-                        options: BoxOptions:: $o_preset (),
-                    }
-                }
-            )*}
-        };
-    }
-
-    impl Container {
-        /// Construct with options set to passed value and other fields default.
-        #[must_use]
-        pub fn with_options(options: BoxOptions) -> Self {
-            Self {
-                options,
-                ..Default::default()
-            }
-        }
-
-        /// Construct with palette set to passed value and other fields default.
-        #[must_use]
-        pub fn with_palette(palette: ContrastPalette) -> Self {
-            Self {
-                palette,
-                ..Default::default()
-            }
-        }
-
-        container_presets![
-            (monochrome, solid),
-            (monochrome, defined),
-            (monochrome, minimal),
-        ];
-    }
-
-    impl From<ContrastPalette> for Container {
-        fn from(value: ContrastPalette) -> Self {
-            Self::with_palette(value)
-        }
-    }
-
-    impl From<BoxOptions> for Container {
-        fn from(value: BoxOptions) -> Self {
-            Self::with_options(value)
-        }
+    /// Style used for [Text][iced::widget::Text] widgets.
+    #[derive(Default, Clone, Copy, Debug)]
+    pub enum Text {
+        /// Use the default style of the current theme.
+        #[default]
+        Theme,
+        /// Use a palette based on contrast swapping what is foreground and background based on
+        /// theme
+        ContrastPalette(ContrastPalette),
+        /// Set the text color to the passed color.
+        Color(Color),
     }
 }
 
@@ -339,12 +344,20 @@ impl iced::application::StyleSheet for Theme {
 }
 
 impl iced::widget::text::StyleSheet for Theme {
-    type Style = theme::Container;
+    type Style = Option<theme::Text>;
 
     fn appearance(&self, style: Self::Style) -> iced::widget::text::Appearance {
-        let palette = BoxPalette::from_contrast_palette(*self, style.palette);
-        iced::widget::text::Appearance {
-            color: style.options.text.then_some(palette.text),
+        use iced::widget::text::Appearance;
+        Appearance {
+            color: style.map(|style| match style {
+                theme::Text::Theme => todo!(),
+                theme::Text::ContrastPalette(palette) => match self {
+                    Theme::Light => palette.dim,
+                    Theme::Dark => palette.bright,
+                    Theme::DarkMute => mute_color(palette.dim, None),
+                },
+                theme::Text::Color(color) => color,
+            }),
         }
     }
 }
@@ -353,17 +366,18 @@ impl iced::widget::container::StyleSheet for Theme {
     type Style = theme::Container;
 
     fn appearance(&self, style: &Self::Style) -> iced::widget::container::Appearance {
-        let palette = BoxPalette::from_contrast_palette(*self, style.palette);
-        iced::widget::container::Appearance {
-            border_radius: 0.0,
-            border_width: if style.options.border { 1.0 } else { 0.0 },
-            text_color: style.options.text.then_some(palette.text),
-            background: style
-                .options
-                .background
-                .then_some(palette.background.into()),
-            border_color: palette.text,
-        }
+        // let palette = BoxPalette::from_contrast_palette(*self, style.palette);
+        // iced::widget::container::Appearance {
+        //     border_radius: 0.0,
+        //     border_width: if style.options.border { 1.0 } else { 0.0 },
+        //     text_color: style.options.text.then_some(palette.text),
+        //     background: style
+        //         .options
+        //         .background
+        //         .then_some(palette.background.into()),
+        //     border_color: palette.text,
+        // }
+        todo!()
     }
 }
 
@@ -371,30 +385,32 @@ impl iced::widget::toggler::StyleSheet for Theme {
     type Style = theme::Container;
 
     fn active(&self, style: &Self::Style, _is_active: bool) -> iced::widget::toggler::Appearance {
-        let palette = BoxPalette::from_contrast_palette(*self, style.palette).pipe(|p| {
-            if style.options.border {
-                p
-            } else {
-                p.invert()
-            }
-        });
-        iced::widget::toggler::Appearance {
-            background: palette.background,
-            foreground: palette.text,
-            background_border: style.options.border.then_some(palette.text),
-            foreground_border: style.options.border.then_some(palette.background),
-        }
+        // let palette = BoxPalette::from_contrast_palette(*self, style.palette).pipe(|p| {
+        //     if style.options.border {
+        //         p
+        //     } else {
+        //         p.invert()
+        //     }
+        // });
+        // iced::widget::toggler::Appearance {
+        //     background: palette.background,
+        //     foreground: palette.text,
+        //     background_border: style.options.border.then_some(palette.text),
+        //     foreground_border: style.options.border.then_some(palette.background),
+        // }
+        todo!()
     }
 
     fn hovered(&self, style: &Self::Style, _is_active: bool) -> iced::widget::toggler::Appearance {
-        let palette = BoxPalette::from_contrast_palette(*self, style.palette.mute_dim(None))
-            .pipe(|p| if style.options.border { p } else { p.invert() });
-        iced::widget::toggler::Appearance {
-            background: palette.background,
-            foreground: palette.text,
-            background_border: style.options.border.then_some(palette.text),
-            foreground_border: style.options.border.then_some(palette.background),
-        }
+        // let palette = BoxPalette::from_contrast_palette(*self, style.palette.mute_dim(None))
+        //     .pipe(|p| if style.options.border { p } else { p.invert() });
+        // iced::widget::toggler::Appearance {
+        //     background: palette.background,
+        //     foreground: palette.text,
+        //     background_border: style.options.border.then_some(palette.text),
+        //     foreground_border: style.options.border.then_some(palette.background),
+        // }
+        todo!()
     }
 }
 
@@ -402,46 +418,48 @@ impl iced::widget::button::StyleSheet for Theme {
     type Style = theme::Container;
 
     fn active(&self, style: &Self::Style) -> iced::widget::button::Appearance {
-        let palette = BoxPalette::from_contrast_palette(*self, style.palette).pipe(|p| {
-            if style.options.border || (!style.options.background) {
-                p
-            } else {
-                p.invert()
-            }
-        });
-        iced::widget::button::Appearance {
-            background: style
-                .options
-                .background
-                .then_some(palette.background.into()),
-            border_radius: 3.0,
-            border_width: if style.options.border { 1.0 } else { 0.0 },
-            border_color: palette.text,
-            text_color: palette.text,
-            ..Default::default()
-        }
+        // let palette = BoxPalette::from_contrast_palette(*self, style.palette).pipe(|p| {
+        //     if style.options.border || (!style.options.background) {
+        //         p
+        //     } else {
+        //         p.invert()
+        //     }
+        // });
+        // iced::widget::button::Appearance {
+        //     background: style
+        //         .options
+        //         .background
+        //         .then_some(palette.background.into()),
+        //     border_radius: 3.0,
+        //     border_width: if style.options.border { 1.0 } else { 0.0 },
+        //     border_color: palette.text,
+        //     text_color: palette.text,
+        //     ..Default::default()
+        // }
+        todo!()
     }
 
     fn hovered(&self, style: &Self::Style) -> iced::widget::button::Appearance {
-        let palette = BoxPalette::from_contrast_palette(*self, style.palette)
-            .pipe(|p| {
-                if style.options.border || (!style.options.background) {
-                    p
-                } else {
-                    p.invert()
-                }
-            })
-            .mute_background(None);
-        iced::widget::button::Appearance {
-            background: style
-                .options
-                .background
-                .then_some(palette.background.into()),
-            border_radius: 3.0,
-            border_width: if style.options.border { 1.0 } else { 0.0 },
-            border_color: palette.text,
-            text_color: palette.text,
-            ..Default::default()
-        }
+        // let palette = BoxPalette::from_contrast_palette(*self, style.palette)
+        //     .pipe(|p| {
+        //         if style.options.border || (!style.options.background) {
+        //             p
+        //         } else {
+        //             p.invert()
+        //         }
+        //     })
+        //     .mute_background(None);
+        // iced::widget::button::Appearance {
+        //     background: style
+        //         .options
+        //         .background
+        //         .then_some(palette.background.into()),
+        //     border_radius: 3.0,
+        //     border_width: if style.options.border { 1.0 } else { 0.0 },
+        //     border_color: palette.text,
+        //     text_color: palette.text,
+        //     ..Default::default()
+        // }
+        todo!()
     }
 }
--- crates/util/src/lib.rs
+++ crates/util/src/lib.rs
@@ -8,6 +8,8 @@
     rustdoc::all
 )]
 
+use std::ops::Deref;
+
 /// Trait extending all types with utility methods.
 pub trait AnyWithExt {
     /// Change state of type with a function and a value if said value is Some else do nothing.
@@ -45,3 +47,38 @@ pub trait AnyWithExt {
 }
 
 impl<T> AnyWithExt for T {}
+
+/// An object that exists "somewhere" be it in a box, or staticaly somwhere else. Primary use is
+/// for trait objects.
+#[derive(Clone, Debug)]
+pub enum Somewhere<T>
+where
+    T: 'static + ?Sized,
+{
+    /// A reference to the value if it exists somewhere else.
+    Borrowed(&'static T),
+    /// A Box to the value if it is owned by self.
+    Owned(Box<T>),
+}
+
+impl<T> From<&'static T> for Somewhere<T>
+where
+    T: 'static + ?Sized,
+{
+    fn from(value: &'static T) -> Self {
+        Self::Borrowed(value)
+    }
+}
+
+impl<T> Deref for Somewhere<T>
+where
+    T: 'static + ?Sized,
+{
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Somewhere::Borrowed(val) => val,
+            Somewhere::Owned(val) => val,
+        }
+    }
+}
